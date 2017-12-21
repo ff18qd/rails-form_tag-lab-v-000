00abbe3999970fe544abd1aa359d0fbb523ec5c5
Emma Feng <ff18qd@gmail.com>
2017-12-21 20:37:27 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,6 @@ class StudentsController < ApplicationController
   end
 
   def create
-    Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    @student=Student.create(first_name: params[:first_name], last_name: params[:last_name])
   end
 end
