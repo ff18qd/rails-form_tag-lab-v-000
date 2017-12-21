07814dc37f71edf30ac7f7a5cd2072cb189790f8
Emma Feng <ff18qd@gmail.com>
2017-12-21 20:32:30 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -8,9 +8,10 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def new 
-  end 
+  def new
+  end
 
   def create
-  end 
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
+  end
 end
